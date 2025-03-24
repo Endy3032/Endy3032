cb4c18951a90adc5b65fe16a09d7f58d8993b1d2
Endy3032 <endy3032@icloud.com>
2025-03-24 22:33:23 +0700

    remove site

--- pages/404.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="m-0 p-0 w-max h-max absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-      <code className="text-3xl text-red font-bold underline m-auto">Oops! 404</code>
-    </div>
-  )
-}
